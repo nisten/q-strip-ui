49f990125a6235a1fd2f3ba1c67cc872536edc69
nisten <nisten@give2science.com>
2022-12-04 16:08:04 -0500

    added layercake, doesnt work yet

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,88 @@
 <script>
   import svelteLogo from './assets/svelte.svg'
   import Counter from './lib/Counter.svelte'
+  import { LayerCake, Svg, Html } from 'layercake';
+
+  import Line from './_components/Line.svelte';
+  import Area from './_components/Area.svelte';
+  import AxisX from './_components/AxisX.svelte';
+  import AxisY from './_components/AxisY.svelte';
+  import Brush from './_components/Brush.html.svelte';
+
+  // This example loads csv data as json using @rollup/plugin-dsv
+  import data from './_data/points.csv';
+
+  let brushExtents = [null, null];
+
+  const xKey = 'myX';
+  const yKey = 'myY';
+
+  data.forEach(d => {
+    d[yKey] = +d[yKey];
+  });
+
+  let brushedData;
+  $: {
+    brushedData = data.slice((brushExtents[0] || 0) * data.length, (brushExtents[1] || 1) * data.length);
+    if (brushedData.length < 2) {
+      brushedData = data.slice(brushExtents[0] * data.length, brushExtents[0] * data.length + 2)
+    }
+  }
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <div class="chart-container">
+    <LayerCake
+      padding={{ right: 10, bottom: 20, left: 25 }}
+      x={xKey}
+      y={yKey}
+      yDomain={[0, null]}
+      data={brushedData}
+    >
+      <Svg>
+        <AxisX
+          ticks={ticks => {
+            const filtered = ticks.filter(t => t % 1 === 0);
+            if (filtered.length > 7) {
+              return filtered.filter((t, i) => i % 2 === 0);
+            }
+            return filtered;
+          }}
+        />
+        <AxisY
+          ticks={4}
+        />
+        <Line
+          stroke='#00e047'
+        />
+        <Area
+          fill='#00e04710'
+        />
+      </Svg>
+    </LayerCake>
   </div>
-  <h1>Vite + Svelte</h1>
+  
+  <div class="brush-container">
+    <LayerCake
+      padding={{ top: 5 }}
+      x={xKey}
+      y={yKey}
+      yDomain={[0, null]}
+      data={data}
+    >
+      <Svg>
+        <Line
+          stroke='#00e047'
+        />
+        <Area
+          fill='#00e04710'
+        />
+      </Svg>
+      <Html>
+        <Brush bind:min={brushExtents[0]} bind:max={brushExtents[1]}/>
+      </Html>
+    </LayerCake>
+  </div>
 
   <div class="card">
     <Counter />
@@ -28,18 +98,15 @@
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
   .read-the-docs {
     color: #888;
+  }
+  .chart-container {
+    width: 100%;
+    height: 80%;
+  }
+  .brush-container {
+    width: 100%;
+    height: 20%;
   }
 </style>
